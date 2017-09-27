4c520c17fe66e9ddaf84286038c8f0202bd957a5
Mitchell Barry <mitch.barry@gmail.com>
2017-09-27 12:17:44 -0500

    tests(gameType): added specialized GameType coverage

---
+++ src/models/gameType/texasHoldEmGameType.spec.ts
@@ -0,0 +1,7 @@
+import { test } from 'ava'
+import { TexasHoldEmPokerGameType } from 'typedeck'
+
+test('TexasHoldEmPokerGameType has allowed cards', async t => {
+  const gameType = new TexasHoldEmPokerGameType()
+  t.true(gameType.cardsAllowed.length > 0, 'TexasHoldEmPokerGameType did not contain Cards at initialization')
+})
