dc706b3f3f91aafde7cd0305ce18f5df0d4de479
Mitchell Barry <mitch.barry@gmail.com>
2017-09-30 12:53:40 -0500

    chore: remove examples. will find better method.

--- examples/node-typescript/src/test.ts
+++
@@ -1,7 +0,0 @@
-// Typescript should resolve this using the same algorithm as Node.js.
-// See examples/node-vanilla for more info.
-import { ICard } from 'typedeck'
-import * as assert from 'assert'
-
-assert(12 === 12)
-console.log('✔ double(6) === 12')
