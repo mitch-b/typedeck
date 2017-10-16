715aedce330df69e7d185b28afbdbd91a65598d3
Mitchell Barry <mitch.barry@gmail.com>
2017-10-16 00:03:37 -0500

    need algorithm improvement for larger scale work

--- src/services/chip.service.spec.ts
+++ src/services/chip.service.spec.ts
@@ -49,21 +49,21 @@ test('can split chips by breaking up a chip and giving smaller denominations', a
   t.true(chipCollection.getValue() === initialValue - requestedAmount, 'Chips left in collection do not match pulled')
 })
 
-// test('can split chips by breaking up a chip and giving smaller denominations complex', async t => {
-//   const service = new ChipService()
-//   const myChips: IChip[] = [
-//     new StandardChip(ChipColor.Black),
-//     new StandardChip(ChipColor.Black),
-//     new StandardChip(ChipColor.Black),
-//     new StandardChip(ChipColor.Black)
-//   ]
-//   const chipCollection = new ChipCollection(myChips)
-//   const initialValue = chipCollection.getValue()
-//   const requestedAmount = 398
-//   const chipsOfRequestedValue = new ChipCollection(service.makeChange(chipCollection, requestedAmount))
-//   t.true(chipsOfRequestedValue.getValue() === requestedAmount, 'Amount pulled did not match requested')
-//   t.true(chipCollection.getValue() === initialValue - requestedAmount, 'Chips left in collection do not match pulled')
-// })
+test('can split chips by breaking up a chip and giving smaller denominations complex', async t => {
+  const service = new ChipService()
+  const myChips: IChip[] = [
+    new StandardChip(ChipColor.Black),
+    new StandardChip(ChipColor.Black),
+    new StandardChip(ChipColor.Black),
+    new StandardChip(ChipColor.Black)
+  ]
+  const chipCollection = new ChipCollection(myChips)
+  const initialValue = chipCollection.getValue()
+  const requestedAmount = 398
+  const chipsOfRequestedValue = new ChipCollection(service.makeChange(chipCollection, requestedAmount))
+  t.true(chipsOfRequestedValue.getValue() === requestedAmount, 'Amount pulled did not match requested')
+  t.true(chipCollection.getValue() === initialValue - requestedAmount, 'Chips left in collection do not match pulled')
+})
 
 test('can create chips from amount 31', async t => {
   const service = new ChipService()
