58406d776c479ae623fb28c484fd5b18fa2a3510
Matthew Hailwood <matthew@hailwood.nz>
2018-03-22 01:58:33 +1300

    chore(build): Enable compilation if --strict compiler option is enabled. (#8)
    Specifically, this corrects issues with the --strictPropertyInitialization option

--- src/models/card/cardRanks.model.ts
+++ src/models/card/cardRanks.model.ts
@@ -3,7 +3,7 @@ import { ICard } from './card.interface'
 import { IRankSet } from './rankSet.interface'
 
 export class RankSet implements IRankSet {
-  public rankSet: CardName[]
+  public rankSet: CardName[] = []
   public cardHigherThan (thisCard: ICard, compareCard: ICard): boolean {
     return this.getRankValue(thisCard) > this.getRankValue(compareCard)
   }
